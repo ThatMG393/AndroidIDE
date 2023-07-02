4fb49eb3b46678b24ad3d26bdf9dff0edf6ac81a
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2023-07-02 17:18:53 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     }
 
     buildTypes {
-      debug { signingConfig = signingConfigs.getByName("common") }
+      debug { signingConfig = signingConfigs.debug }
       release { signingConfig = signingConfigs.getByName("common") }
     }
   } else {
